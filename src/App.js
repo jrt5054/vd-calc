2c5ffbed5810808515da7e5f86da78094d29c9a0
Joshua Taylor <JoshuaRTaylor87@gmail.com>
2021-11-20 11:13:14 -0500

    complete VD Calc

--- src/App.js
+++ src/App.js
@@ -31,58 +31,74 @@ class App extends React.Component {
         "#1000",
       ],
       listOfReactances: [
-        { num14: { PVCAl: 0.058, steel: 0.073 } },
-        { num12: { PVCAl: 0.054, steel: 0.068 } },
+        { wire: "num14", PVC: 0.058, aluminum: 0.058, steel: 0.073 },
+        { wire: "num12", PVC: 0.054, aluminum: 0.054, steel: 0.068 },
+        { wire: "num10", PVC: 0.05, aluminum: 0.05, steel: 0.063 },
+        { wire: "num8", PVC: 0.052, aluminum: 0.052, steel: 0.065 },
+        { wire: "num6", PVC: 0.051, aluminum: 0.051, steel: 0.064 },
+        { wire: "num4", PVC: 0.048, aluminum: 0.048, steel: 0.06 },
+        { wire: "num3", PVC: 0.047, aluminum: 0.047, steel: 0.059 },
+        { wire: "num2", PVC: 0.045, aluminum: 0.045, steel: 0.057 },
+        { wire: "num1", PVC: 0.046, aluminum: 0.046, steel: 0.057 },
+        { wire: "num1/0", PVC: 0.044, aluminum: 0.044, steel: 0.055 },
+        { wire: "num2/0", PVC: 0.043, aluminum: 0.043, steel: 0.054 },
+        { wire: "num3/0", PVC: 0.042, aluminum: 0.042, steel: 0.052 },
+        { wire: "num4/0", PVC: 0.041, aluminum: 0.041, steel: 0.051 },
+        { wire: "num250", PVC: 0.041, aluminum: 0.041, steel: 0.052 },
+        { wire: "num300", PVC: 0.041, aluminum: 0.041, steel: 0.051 },
+        { wire: "num350", PVC: 0.04, aluminum: 0.04, steel: 0.05 },
+        { wire: "num400", PVC: 0.04, aluminum: 0.04, steel: 0.049 },
+        { wire: "num500", PVC: 0.039, aluminum: 0.039, steel: 0.048 },
+        { wire: "num600", PVC: 0.039, aluminum: 0.039, steel: 0.048 },
+        { wire: "num750", PVC: 0.038, aluminum: 0.038, steel: 0.048 },
+        { wire: "num1000", PVC: 0.037, aluminum: 0.037, steel: 0.046 },
       ],
       listOfCuACResistances: [
-        { num14: { PVC: 3.1, aluminum: 3.1, steel: 3.1 } },
-        { num12: { PVC: 2, aluminum: 2, steel: 2 } },
+        { wire: "num14", PVC: 3.1, aluminum: 3.1, steel: 3.1 },
+        { wire: "num12", PVC: 2, aluminum: 2, steel: 2 },
+        { wire: "num10", PVC: 1.2, aluminum: 1.2, steel: 1.2 },
+        { wire: "num8", PVC: 0.78, aluminum: 0.78, steel: 0.78 },
+        { wire: "num6", PVC: 0.49, aluminum: 0.49, steel: 0.49 },
+        { wire: "num4", PVC: 0.31, aluminum: 0.31, steel: 0.31 },
+        { wire: "num3", PVC: 0.25, aluminum: 0.25, steel: 0.25 },
+        { wire: "num2", PVC: 0.19, aluminum: 0.2, steel: 0.2 },
+        { wire: "num1", PVC: 0.15, aluminum: 0.16, steel: 0.16 },
+        { wire: "num1/0", PVC: 0.12, aluminum: 0.13, steel: 0.12 },
+        { wire: "num2/0", PVC: 0.1, aluminum: 0.1, steel: 0.1 },
+        { wire: "num3/0", PVC: 0.077, aluminum: 0.082, steel: 0.079 },
+        { wire: "num4/0", PVC: 0.062, aluminum: 0.067, steel: 0.063 },
+        { wire: "num250", PVC: 0.052, aluminum: 0.057, steel: 0.054 },
+        { wire: "num300", PVC: 0.044, aluminum: 0.049, steel: 0.045 },
+        { wire: "num350", PVC: 0.038, aluminum: 0.043, steel: 0.039 },
+        { wire: "num400", PVC: 0.033, aluminum: 0.038, steel: 0.035 },
+        { wire: "num500", PVC: 0.027, aluminum: 0.032, steel: 0.029 },
+        { wire: "num600", PVC: 0.023, aluminum: 0.028, steel: 0.025 },
+        { wire: "num750", PVC: 0.019, aluminum: 0.024, steel: 0.021 },
+        { wire: "num1000", PVC: 0.015, aluminum: 0.019, steel: 0.018 }
       ],
       listOfAlACResistances: [
-        { num14: { PVC: 3.2, aluminum: 3.2, steel: 3.2 } },
-        { num12: { PVC: 2, aluminum: 2, steel: 2 } },
-      ],
-      infoFromForm: {
-        wireTag: "",
-        load: "",
-        loadType: "",
-        pf: "",
-        voltage: "",
-        numOfPhases: "",
-        conductorMaterial: "",
-        conduitMaterial: "",
-        parallelRuns: "",
-        wireSize: "",
-        wireLength: "",
-      },
-      voltageDropItemsArray: [
-        {
-          wireTag: "1a",
-          load: "20",
-          loadType: "Amps",
-          pf: "0.85",
-          voltage: "480",
-          numOfPhases: "Three-Phase",
-          conductorMaterial: "Copper",
-          conduitMaterial: "Steel",
-          parallelRuns: "1",
-          wireSize: "#6",
-          wireLength: "150",
-        },
-        {
-          wireTag: "2a",
-          load: "30",
-          loadType: "Amps",
-          pf: "0.85",
-          voltage: "480",
-          numOfPhases: "Three-Phase",
-          conductorMaterial: "Copper",
-          conduitMaterial: "Steel",
-          parallelRuns: "1",
-          wireSize: "#1",
-          wireLength: "200",
-        },
-      ],
+        // { wire: "num14", PVC: 0., aluminum: 0., steel: 0. }, this needs to be handled becasue there is no value for #14 aluminum wire
+        { wire: "num12", PVC: 3.2, aluminum: 3.2, steel: 3.2 },
+        { wire: "num10", PVC: 2, aluminum: 2, steel: 2 },
+        { wire: "num8", PVC: 1.3, aluminum: 1.3, steel: 1.3 },
+        { wire: "num6", PVC: 0.81, aluminum: 0.81, steel: 0.81 },
+        { wire: "num4", PVC: 0.51, aluminum: 0.51, steel: 0.51 },
+        { wire: "num3", PVC: 0.4, aluminum: 0.41, steel: 0.4 },
+        { wire: "num2", PVC: 0.32, aluminum: 0.32, steel: 0.32 },
+        { wire: "num1", PVC: 0.25, aluminum: 0.26, steel: 0.25 },
+        { wire: "num1/0", PVC: 0.2, aluminum: 0.21, steel: 0.2 },
+        { wire: "num2/0", PVC: 0.16, aluminum: 0.16, steel: 0.16 },
+        { wire: "num3/0", PVC: 0.13, aluminum: 0.13, steel: 0.13 },
+        { wire: "num4/0", PVC: 0.1, aluminum: 0.11, steel: 0.1 },
+        { wire: "num250", PVC: 0.085, aluminum: 0.09, steel: 0.086 },
+        { wire: "num300", PVC: 0.071, aluminum: 0.076, steel: 0.072 },
+        { wire: "num350", PVC: 0.061, aluminum: 0.066, steel: 0.063 },
+        { wire: "num400", PVC: 0.054, aluminum: 0.059, steel: 0.055 },
+        { wire: "num500", PVC: 0.043, aluminum: 0.048, steel: 0.045 },
+        { wire: "num600", PVC: 0.036, aluminum: 0.041, steel: 0.038 },
+        { wire: "num750", PVC: 0.029, aluminum: 0.034, steel: 0.031 },
+        { wire: "num1000", PVC: 0.023, aluminum: 0.027, steel: 0.025 }
+      ]
     };
     this.calcVD = this.calcVD.bind(this);
   }
@@ -90,71 +106,97 @@ class App extends React.Component {
   calcImp(impInput) {
     // simplify variable names using destructuring syntax
     const { wireSize, conductorMaterial, conduitMaterial, pf } = impInput;
+    const { listOfAlACResistances, listOfCuACResistances, listOfReactances } =
+      this.state;
+    // initialize variables since they are only generated inside if statements
+    let resistance;
+    let reactance;
 
-    // convert wire format from "#" to "num" by slicing out the first character and concatinating "num"
+    // convert wire format from "#" to "num" by removing the first character (#) and concatinating "num"
     const numWireSizeFromForm = "num" + wireSize.slice(1);
-    console.log(`this is numbWireSize ${numWireSizeFromForm}`)
 
     // assign the correct value of the resistance by checking if the value of the conductor material from the form is copper. if it is not, it must be aluminum. take the values from the appropriate location of the state object.
     if (conductorMaterial === "copper") {
-      // Search through listOfACResistances keys for numWireSize match.
-      // Once found search through the value of that key (which is an object) for the key of the conduit material. return that value
-      const resistance =
-        this.state.listOfCuACResistances.find((singleElement)=>{
-          
-          return singleElement.numWireSizeFromForm == numWireSizeFromForm
-        })
-        console.log(`this is the resistance: ${resistance}`)
+      // use find() to search through the array for objects with a wire property value of what is stored in numWireSizeFromForm. what is returned is the object with the corresponding wire property
+      // Once found, pull out the correct value by searching for the property of conduitMaterial and assigning it to 'resistance'
+      let wireProperties = listOfCuACResistances.find((element) => {
+        return element.wire === numWireSizeFromForm;
+      });
+      resistance = wireProperties[conduitMaterial];
+    } else {
+      let wireProperties = listOfAlACResistances.find((element) => {
+        return element.wire === numWireSizeFromForm;
+      });
+      resistance = wireProperties[conduitMaterial];
     }
-    // } else {
-    //   const resistance =
-    //     this.state.listOfAlACResistances.numWireSize.conduitMaterial;
-    // }
 
-    // assign the correct value of the reactance by checking if the value of the conduit material from the form is either PVC or Aluminum. if it is not, it must be steel. take the values from the appropriate location of the state object. Note: reactance values for PVC and aluminum are the same from Table 9 of the NEC
-    // if (conduitMaterial === "PVC" || conduitMaterial === "aluminum") {
-    //   const reactance = this.state.listOfReactances.numWireSize.PVCAl;
-    // } else {
-    //   const reactance = this.state.listOfReactances.numWireSize.steel;
-    // }
+    // use find() to search through the array for objects with a wire property value of what is stored in numWireSizeFromForm. what is returned is the object with the corresponding wire property
+    // Once found, pull out the correct value by searching for the property of conduitMaterial and assigning it to 'reactance'
+    let wireProperties = listOfReactances.find((element) => {
+      return element.wire === numWireSizeFromForm;
+    });
+    reactance = wireProperties[conduitMaterial];
 
     // calculate the impedance using the following equation: impedance = (Resistance * cos(arccos(pf)))+(Reactance * sin(arccos(pf))). Note: arccos(pf) is the angle of the power triangle.
-    // let impedance =
-    //   resistance * Math.cos(Math.acos(pf)) +
-    //   reactance * Math.sin(Math.acos(pf));
+    let impedance =
+      resistance * Math.cos(Math.acos(pf)) +
+      reactance * Math.sin(Math.acos(pf));
 
-    // return impedance;
+    console.log(`Reactance: ${reactance}`);
+    console.log(`Resistance: ${resistance}`);
+    console.log(`Impedance: ${impedance}`);
+
+    return impedance;
   }
 
-  // calcAmps(ampInput) {
-  //   // simplify variable names using destructuring syntax
-  //   const { load, loadType, phase, voltage, pf } = ampInput;
+  calcAmps(ampInput) {
+    // simplify variable names using destructuring syntax
+    let { load, loadType, numOfPhases, voltage, pf } = ampInput;
 
-  //   // if amps return amps
-  //   if (loadType === "amps") {
-  //     return load;
-  //   }
-  //   // if watts and single phase return watts/(voltage * pf)
-  //   if (loadType === "watts" && phase === "single") {
-  //     return load / (voltage * pf);
-  //   }
-  //   // if watts and three phase return watts/(voltage * pf * 1.73)
-  //   if (loadType === "watts" && phase === "three") {
-  //     return load / (voltage * pf * 1.73);
-  //   }
-  //   // if VA and single phase return VA/voltage
-  //   if (loadType === "voltAmps" && phase === "single") {
-  //     return load / voltage;
-  //   }
-  //   // if VA and three phase return VA/(voltage*1.73)
-  //   if (loadType === "voltAmps" && phase === "three") {
-  //     return load / (voltage * 1.73);
-  //   }
-  // }
+    // if amps return amps
+    if (loadType === "amps") {
+      // console.log(`Load type should be Amps: ${loadType}`)
+      // console.log(`Load was: ${load}A`)
+      // console.log(`Load data type was: ${typeof(load)}`)
+      return load;
+    }
+    // if watts and single phase return watts/(voltage * pf)
+    if (loadType === "watts" && numOfPhases === "single") {
+      // console.log(`Load type should be watts: ${loadType}`)
+      // console.log(`Load was: ${load}W`)
+      // console.log(`number of phases was: ${numOfPhases}`)
+      // console.log(`Load data type was: ${typeof(load)}`)
+      return load / (voltage * pf);
+    }
+    // if watts and three phase return watts/(voltage * pf * 1.73)
+    if (loadType === "watts" && numOfPhases === "three") {
+      // console.log(`Load type should be watts: ${loadType}`)
+      // console.log(`Load was: ${load}W`)
+      // console.log(`number of phases was: ${numOfPhases}`)
+      // console.log(`Load data type was: ${typeof(load)}`)
+      return load / (voltage * pf * 1.73);
+    }
+    // if VA and single phase return VA/voltage
+    if (loadType === "voltAmps" && numOfPhases === "single") {
+      // console.log(`Load type should be volt-amps: ${loadType}`)
+      // console.log(`Load was: ${load}VA`)
+      // console.log(`number of phases was: ${numOfPhases}`)
+      // console.log(`Load data type was: ${typeof(load)}`)
+      return load / voltage;
+    }
+    // if VA and three phase return VA/(voltage*1.73)
+    if (loadType === "voltAmps" && numOfPhases === "three") {
+      // console.log(`Load type should be volt-amps: ${loadType}`)
+      // console.log(`Load was: ${load}VA`)
+      // console.log(`number of phases was: ${numOfPhases}`)
+      // console.log(`Load data type was: ${typeof(load)}`)
+      return load / (voltage * 1.73);
+    }
+  }
 
   calcVD(newVDItemInfo) {
     // simplify variable names using destructuring syntax
-    const {
+    let {
       wireTag,
       load,
       loadType,
@@ -167,34 +209,40 @@ class App extends React.Component {
       wireSize,
       wireLength,
     } = newVDItemInfo;
+    let voltageDrop;
 
+    console.log(`Wire Tag: ${wireTag}`);
     // create an input object for the function calls to simplify code
     const impInput = { wireSize, conductorMaterial, conduitMaterial, pf };
-    // const ampInput = { load, loadType, phase, voltage, pf };
+    const ampInput = { load, loadType, numOfPhases, voltage, pf };
 
     // calculate the impedance
     const impedance = this.calcImp(impInput);
+    // console.log(`this is the impedance from calcImp(): ${impedance}`)
 
     // calculate the amperage based on the form data
-    // const amps = this.calcAmps(ampInput);
+    const amps = this.calcAmps(ampInput);
+    // console.log(`this is the amps from calcAmps(): ${amps}A`)
 
-    // // 1 phase VD = impedance*(dist/1000)*Amps*2/(num parallel runs)
-    // // 3 phase VD = impedance*(dist/1000)*Amps*1.73/(num parallel runs)
-    // // console.log(vdInfo);
-    // if (numOfPhases === "single") {
-    //   let voltageDrop =
-    //     (impedance * (wireLength / 1000) * amps * 2) / parallelRuns;
-    // } else {
-    //   let voltageDrop =
-    //     (impedance * (wireLength / 1000) * amps * 1.73) / parallelRuns;
-    // }
+    // 1 phase VD = impedance*(dist/1000)*Amps*2/(num parallel runs)
+    // 3 phase VD = impedance*(dist/1000)*Amps*1.73/(num parallel runs)
+    // console.log(vdInfo);
+    if (numOfPhases === "single") {
+      voltageDrop = (impedance * (wireLength / 1000) * amps * 2) / parallelRuns;
+    } else {
+      voltageDrop =
+        (impedance * (wireLength / 1000) * amps * 1.73) / parallelRuns;
+    }
 
-    // let voltageDropPercent = (voltageDrop / voltage) * 100;
+    let voltageDropPercent = (voltageDrop / voltage) * 100;
 
-    // return {
-    //   voltageDrop: voltageDrop,
-    //   voltageDropPercent: voltageDropPercent,
-    // };
+    console.log(`Voltage Drop: ${voltageDrop}`);
+    console.log(`Voltage Drop Percent: ${voltageDropPercent}`);
+
+    return {
+      voltageDrop: voltageDrop,
+      voltageDropPercent: voltageDropPercent,
+    };
   }
 
   render() {
--- src/Form.js
+++ src/Form.js
@@ -14,16 +14,16 @@ class Form extends React.Component {
     let newVDItemInfo = {
       id: this.uniqueId,
       wireTag: event.target.wireTag.value,
-      load: event.target.load.value,
+      load: Number(event.target.load.value),
       loadType: event.target.loadType.value,
-      pf: event.target.pf.value,
+      pf: Number(event.target.pf.value),
       voltage: event.target.voltage.value,
       numOfPhases: event.target.numOfPhases.value,
       conductorMaterial: event.target.conductorMaterial.value,
       conduitMaterial: event.target.conduitMaterial.value,
-      parallelRuns: event.target.parallelRuns.value,
+      parallelRuns: Number(event.target.parallelRuns.value),
       wireSize: event.target.wireSize.value,
-      wireLength: event.target.wireLength.value
+      wireLength: Number(event.target.wireLength.value)
       }
     this.props.calcVD(newVDItemInfo)
 
@@ -33,29 +33,29 @@ class Form extends React.Component {
     return (
       <div className="container-fluid pt-3 pb-3">
         <form onSubmit={this.handleSubmit}>
-          <div class="row mb-3">
-            <label htmlFor="wireTag" class="col-sm-3 col-form-label">
+          <div className="row mb-3">
+            <label htmlFor="wireTag" className="col-sm-3 col-form-label">
               Wire Tag:
             </label>
-            <div class="col-sm-5">
-              <input type="text" class="form-control" id="wireTag" />
+            <div className="col-sm-5">
+              <input type="text" className="form-control" id="wireTag" defaultValue="1a" />
             </div>
           </div>
 
-          <div class="row mb-3">
-            <label htmlFor="load" class="col-sm-3 col-form-label">
+          <div className="row mb-3">
+            <label htmlFor="load" className="col-sm-3 col-form-label">
               Electrical Load:
             </label>
-            <div class="col-sm-5">
-              <input type="text" class="form-control" id="load" />
+            <div className="col-sm-5">
+              <input type="text" className="form-control" id="load" defaultValue="10" />
             </div>
           </div>
 
-          <div class="row mb-3">
-            <label htmlFor="loadType" class="col-sm-3 col-form-label">
+          <div className="row mb-3">
+            <label htmlFor="loadType" className="col-sm-3 col-form-label">
               Load Type:
             </label>
-            <div class="col-sm-5">
+            <div className="col-sm-5">
               <select id="loadType" className="form-select">
                 <option value="amps">Amps</option>
                 <option value="watts">Watts</option>
@@ -64,46 +64,46 @@ class Form extends React.Component {
             </div>
           </div>
 
-          <div class="row mb-3">
-            <label htmlFor="pf" class="col-sm-3 col-form-label">
+          <div className="row mb-3">
+            <label htmlFor="pf" className="col-sm-3 col-form-label">
               Power Factor:
             </label>
-            <div class="col-sm-5">
-              <input type="text" class="form-control" id="pf" />
+            <div className="col-sm-5">
+              <input type="text" className="form-control" id="pf" defaultValue="0.8"/>
             </div>
           </div>
 
-          <div class="row mb-3">
-            <label htmlFor="voltage" class="col-sm-3 col-form-label">
+          <div className="row mb-3">
+            <label htmlFor="voltage" className="col-sm-3 col-form-label">
               Voltage:
             </label>
-            <div class="col-sm-5">
-              <select id="voltage" className="form-select">
-                <option value="120v">120V</option>
-                <option value="208v">208V</option>
-                <option value="277v">277V</option>
-                <option value="480v">480V</option>
+            <div className="col-sm-5">
+              <select id="voltage" className="form-select" defaultValue="480">
+                <option value="120">120V</option>
+                <option value="208">208V</option>
+                <option value="277">277V</option>
+                <option value="480">480V</option>
               </select>
             </div>
           </div>
 
-          <div class="row mb-3">
-            <label htmlFor="numOfPhases" class="col-sm-3 col-form-label">
+          <div className="row mb-3">
+            <label htmlFor="numOfPhases" className="col-sm-3 col-form-label">
               Number of Phases:
             </label>
-            <div class="col-sm-5">
-              <select id="numOfPhases" className="form-select">
+            <div className="col-sm-5">
+              <select id="numOfPhases" className="form-select" defaultValue="three">
                 <option value="single">Single Phase</option>
                 <option value="three">Three Phase</option>
               </select>
             </div>
           </div>
 
-          <div class="row mb-3">
-            <label htmlFor="conductorMaterial" class="col-sm-3 col-form-label">
+          <div className="row mb-3">
+            <label htmlFor="conductorMaterial" className="col-sm-3 col-form-label">
               Conductor Material:
             </label>
-            <div class="col-sm-5">
+            <div className="col-sm-5">
               <select id="conductorMaterial" className="form-select">
                 <option value="copper">Copper</option>
                 <option value="aluminum">Aluminum</option>
@@ -111,11 +111,11 @@ class Form extends React.Component {
             </div>
           </div>
 
-          <div class="row mb-3">
-            <label htmlFor="conduitMaterial" class="col-sm-3 col-form-label">
+          <div className="row mb-3">
+            <label htmlFor="conduitMaterial" className="col-sm-3 col-form-label">
               Conductor Material:
             </label>
-            <div class="col-sm-5">
+            <div className="col-sm-5">
               <select id="conduitMaterial" className="form-select">
                 <option value="steel">Steel</option>
                 <option value="aluminum">Aluminum</option>
@@ -124,21 +124,21 @@ class Form extends React.Component {
             </div>
           </div>
 
-          <div class="row mb-3">
-            <label htmlFor="parallelRuns" class="col-sm-3 col-form-label">
+          <div className="row mb-3">
+            <label htmlFor="parallelRuns" className="col-sm-3 col-form-label">
               Number of Parallel Runs:
             </label>
-            <div class="col-sm-5">
-              <input type="text" class="form-control" id="parallelRuns" />
+            <div className="col-sm-5">
+              <input type="text" className="form-control" id="parallelRuns" defaultValue="1" />
             </div>
           </div>
 
-          <div class="row mb-3">
-            <label htmlFor="wireSize" class="col-sm-3 col-form-label">
+          <div className="row mb-3">
+            <label htmlFor="wireSize" className="col-sm-3 col-form-label">
               Wire Size:
             </label>
-            <div class="col-sm-5">
-              <select id="wireSize" className="form-select">
+            <div className="col-sm-5">
+              <select id="wireSize" className="form-select" defaultValue="#12">
                 {this.props.listOfWireSizes.map((element, i) => {
                   return (
                     <option key={i} value={element}>
@@ -150,17 +150,17 @@ class Form extends React.Component {
             </div>
           </div>
 
-          <div class="row mb-3">
-            <label htmlFor="wireLength" class="col-sm-3 col-form-label">
+          <div className="row mb-3">
+            <label htmlFor="wireLength" className="col-sm-3 col-form-label">
               Wire Length:
             </label>
-            <div class="col-sm-5">
-              <input type="text" class="form-control" id="wireLength" />
+            <div className="col-sm-5">
+              <input type="text" className="form-control" id="wireLength" defaultValue="100" />
             </div>
           </div>
 
-          <div class="col-12">
-            <button type="submit" class="btn btn-primary">Add</button>
+          <div className="col-12">
+            <button type="submit" className="btn btn-primary">Add</button>
             </div>
 
         </form>
